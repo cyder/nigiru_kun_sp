47b8f08609b54c23a5d3b4712dcb595ffb236a40
Kazuki Chigita <kazuki.chigichan@gmail.com>
2018-11-15 01:58:36 +0900

    intrudouce rx and scan only nigirukun

--- lib/datasources/bluetooth/central_manager.dart
+++ lib/datasources/bluetooth/central_manager.dart
@@ -1,27 +1,43 @@
 import 'dart:async';
+import 'dart:core';
+import 'package:rxdart/subjects.dart';
+import 'package:rxdart/rxdart.dart';
 
 import 'package:flutter_blue/flutter_blue.dart';
 
 class CentralManager {
   FlutterBlue _flutterBlue = FlutterBlue.instance;
   StreamSubscription _scanSubscription;
-  Map<DeviceIdentifier, ScanResult> scanResults = new Map();
+  PublishSubject<ScanResult> _subject = PublishSubject<ScanResult>();
   static const String _NIGIRU_KUN_SERVICE =
-      "0000180F-0000-1000-8000-00805F9B34FB";
+      "1db31f9a-9137-44da-9458-a4e642211773";
 
-  startScan() {
-    _scanSubscription = _flutterBlue.scan(
-        timeout: const Duration(seconds: 30),
-        /*withServices: [new Guid(_NIGIRU_KUN_SERVICE)]*/).listen((scanResult) {
+  Observable<ScanResult> get scannedDevice =>
+      _subject.stream.where((scanResult) =>
+          scanResult.advertisementData.serviceUuids.first ==
+          _NIGIRU_KUN_SERVICE);
+
+  startDeviceScan([int timeout = 10]) {
+    if (_subject.isClosed) {
+      _subject = PublishSubject<ScanResult>();
+    }
+    _scanSubscription = _flutterBlue
+        .scan(
+      timeout: Duration(seconds: timeout),
+    )
+        .listen((scanResult) {
       print('localName: ${scanResult.advertisementData.localName}');
-      print('manufacturerData: ${scanResult.advertisementData.manufacturerData}');
+      print('connectable: ${scanResult.advertisementData.connectable}');
+      print(
+          'manufacturerData: ${scanResult.advertisementData.manufacturerData}');
       print('serviceData: ${scanResult.advertisementData.serviceData}');
-      scanResults[scanResult.device.id] = scanResult;
+      _subject.add(scanResult);
     }, onDone: _stopScan);
   }
 
   _stopScan() {
     _scanSubscription?.cancel();
     _scanSubscription = null;
+    _subject.close();
   }
 }
