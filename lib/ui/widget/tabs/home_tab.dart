7a58d6727b3e96bd20fb6226b276702cc068cc64
Mori Atsushi <mori@cyder.tech>
2018-11-15 18:56:26 +0900

    円を表示した

--- lib/ui/widget/tabs/home_tab.dart
+++ lib/ui/widget/tabs/home_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'package:nigiru_kun/viewmodels/home_tab_view_model.dart';
 
@@ -9,6 +10,8 @@ class HomeTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+
     return new ScopedModel<HomeTabViewModel>(
       model: viewModel,
       child: new ScopedModelDescendant<HomeTabViewModel>(
@@ -24,6 +27,50 @@ class HomeTab extends StatelessWidget {
                     'Today ${model.today}',
                     style: const TextStyle(fontSize: 28.0),
                   )),
+                  new CircularPercentIndicator(
+                    radius: size.width * 0.8,
+                    lineWidth: 30.0,
+                    animation: true,
+                    percent: 0.75,
+                    center: new Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        new Row(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.baseline,
+                          textBaseline: TextBaseline.alphabetic,
+                          children: [
+                            new Text(
+                              "75",
+                              style: new TextStyle(fontSize: 50.0),
+                            ),
+                            new Text(
+                              " / 100 回",
+                              style: new TextStyle(fontSize: 20.0),
+                            ),
+                          ],
+                        ),
+                        new Row(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.baseline,
+                          textBaseline: TextBaseline.alphabetic,
+                          children: [
+                            new Text(
+                              "75",
+                              style: new TextStyle(fontSize: 50.0),
+                            ),
+                            new Text(
+                              " %",
+                              style: new TextStyle(fontSize: 20.0),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    circularStrokeCap: CircularStrokeCap.round,
+                    progressColor: Color(0xFFC54244),
+                    backgroundColor: Color(0x33C54244),
+                  ),
                 ]),
               ))),
     );
