2eaee25de42032540f15ac1e3fea069c325958cc
Mori Atsushi <mori@cyder.tech>
2018-11-15 15:56:21 +0900

    日付を表示させるようにした

--- lib/ui/widget/tabs/home_tab.dart
+++ lib/ui/widget/tabs/home_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
 import 'package:nigiru_kun/viewmodels/home_tab_view_model.dart';
 
 class HomeTab extends StatelessWidget {
@@ -8,6 +9,23 @@ class HomeTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new Center(child: new Text('HOME'));
+    return new ScopedModel<HomeTabViewModel>(
+      model: viewModel,
+      child: new ScopedModelDescendant<HomeTabViewModel>(
+          builder: (context, child, model) => new SingleChildScrollView(
+                  child: new Padding(
+                padding: EdgeInsets.symmetric(
+                  vertical: 30.0,
+                  horizontal: 20.0,
+                ),
+                child: new Column(children: [
+                  new Center(
+                      child: new Text(
+                    'Today ${model.today}',
+                    style: const TextStyle(fontSize: 28.0),
+                  )),
+                ]),
+              ))),
+    );
   }
 }
--- lib/viewmodels/home_tab_view_model.dart
+++ lib/viewmodels/home_tab_view_model.dart
@@ -1,3 +1,10 @@
+import 'package:intl/intl.dart';
 import 'package:scoped_model/scoped_model.dart';
 
-class HomeTabViewModel extends Model {}
+class HomeTabViewModel extends Model {
+  String get today {
+    final today = DateTime.now();
+    var formatter = new DateFormat('yyyy.MM.dd');
+    return formatter.format(today);
+  }
+}
