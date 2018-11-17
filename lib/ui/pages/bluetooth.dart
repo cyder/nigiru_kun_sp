d55778eabbbcde96ceded88851ac5e94a9a6d5db
Mori Atsushi <mori@cyder.tech>
2018-11-17 23:44:12 +0900

    Bluetoothの接続仮デザイン挿入

--- lib/ui/pages/bluetooth.dart
+++ lib/ui/pages/bluetooth.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
+
 import 'package:nigiru_kun/viewmodels/bluetooth_view_model.dart';
+import 'package:nigiru_kun/utils/color.dart';
 
 class BluetoothPage extends StatelessWidget {
   final viewModel = new BluetoothViewModel();
@@ -11,13 +13,37 @@ class BluetoothPage extends StatelessWidget {
       model: viewModel,
       child: new ScopedModelDescendant<BluetoothViewModel>(
           builder: (context, child, model) => GestureDetector(
-              onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
-              child: Scaffold(
-                appBar: new AppBar(
-                  title: Text('にぎるくんと接続'),
-                ),
-                body: Text('test'),
-              ))),
+                onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
+                child: Scaffold(
+                    appBar: new AppBar(
+                      title: Text('にぎるくんと接続'),
+                    ),
+                    body: Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 30.0,
+                        horizontal: 20.0,
+                      ),
+                      child: new Column(
+                        children: [
+                          Expanded(child: Text(model.message)),
+                          RaisedButton(
+                            onPressed: model.handleButton,
+                            color: CustomColors.primaryColor,
+                            child: FractionallySizedBox(
+                              widthFactor: 1,
+                              child: Container(
+                                  alignment: Alignment.center,
+                                  margin: EdgeInsets.symmetric(vertical: 16),
+                                  child: Text(
+                                    model.buttonLabel,
+                                    style: TextStyle(fontSize: 16.0),
+                                  )),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )),
+              )),
     );
   }
 }
--- lib/viewmodels/bluetooth_view_model.dart
+++ lib/viewmodels/bluetooth_view_model.dart
@@ -1,3 +1,64 @@
 import 'package:scoped_model/scoped_model.dart';
 
-class BluetoothViewModel extends Model {}
+enum BluetoothState {
+  Connected,
+  Disconnected,
+  Searching,
+}
+
+class BluetoothViewModel extends Model {
+  BluetoothState _currentState = BluetoothState.Disconnected;
+
+  BluetoothState get currentState => _currentState;
+
+  String get message {
+    switch (currentState) {
+      case BluetoothState.Connected:
+        return '接続されました。';
+      case BluetoothState.Disconnected:
+        return '接続されていません。';
+      case BluetoothState.Searching:
+        return '検索中';
+    }
+    return null;
+  }
+
+  String get buttonLabel {
+    switch (currentState) {
+      case BluetoothState.Connected:
+        return '切断';
+      case BluetoothState.Disconnected:
+        return '接続する';
+      case BluetoothState.Searching:
+        return 'キャンセル';
+    }
+    return null;
+  }
+
+  Function get handleButton {
+    switch (currentState) {
+      case BluetoothState.Connected:
+        return disconnect;
+      case BluetoothState.Disconnected:
+        return search;
+      case BluetoothState.Searching:
+        return cancel;
+    }
+    return null;
+  }
+
+  void search() {
+    _currentState = BluetoothState.Connected;
+    notifyListeners();
+  }
+
+  void disconnect() {
+    _currentState = BluetoothState.Disconnected;
+    notifyListeners();
+  }
+
+  void cancel() {
+    _currentState = BluetoothState.Disconnected;
+    notifyListeners();
+  }
+}
