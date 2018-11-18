7a471d70d564fe91f071d120c396e272293867f0
Mori Atsushi <mori@cyder.tech>
2018-11-18 18:26:58 +0900

    HOMEモードのグラフを作成

--- lib/ui/widget/tabs/record_tab.dart
+++ lib/ui/widget/tabs/record_tab.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+
 import 'package:nigiru_kun/viewmodels/record_tab_view_mode.dart';
+import 'package:nigiru_kun/ui/widget/charts/bar_times_chart.dart';
+import 'package:nigiru_kun/utils/color.dart';
 
 class RecordTab extends StatelessWidget {
   final RecordTabViewModel viewModel;
@@ -8,6 +12,40 @@ class RecordTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new Center(child: new Text('Record'));
+    return ScopedModel<RecordTabViewModel>(
+      model: viewModel,
+      child: ScopedModelDescendant<RecordTabViewModel>(
+          builder: (context, child, model) => SingleChildScrollView(
+                  child: Padding(
+                padding: EdgeInsets.symmetric(
+                  vertical: 30.0,
+                  horizontal: 20.0,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'HOMEモード',
+                      style: TextStyle(
+                        fontSize: 32.0,
+                        color: CustomColors.primaryColor,
+                      ),
+                    ),
+                    Container(
+                      height: 200.0,
+                      margin: EdgeInsets.only(bottom: 20.0),
+                      child: BarTimesChart(model.homeSeriesList),
+                    ),
+                    Text(
+                      'Challengeモード',
+                      style: TextStyle(
+                        fontSize: 32.0,
+                        color: CustomColors.secondaryColor,
+                      ),
+                    ),
+                  ],
+                ),
+              ))),
+    );
   }
 }
--- lib/viewmodels/record_tab_view_mode.dart
+++ lib/viewmodels/record_tab_view_mode.dart
@@ -1,3 +1,51 @@
+import 'package:intl/intl.dart';
 import 'package:scoped_model/scoped_model.dart';
+import 'package:charts_flutter/flutter.dart';
+import 'dart:math' as math;
+
+class HomeData {
+  final DateTime date;
+  final int count;
+
+  HomeData(this.date, this.count);
+}
+
+class ChallengeData {
+  final DateTime date;
+  final double force;
+
+  ChallengeData(this.date, this.force);
+}
+
+class RecordTabViewModel extends Model {
+  final formatter = new DateFormat('dd');
+
+  List<Series<HomeData, DateTime>> _homeSeriesList;
+
+  List<Series<HomeData, DateTime>> get homeSeriesList => _homeSeriesList;
+
 
 class RecordTabViewModel extends Model {}
+
+  RecordTabViewModel() {
+    final random = math.Random();
+    final List<HomeData> homeData = []; // TODO: 仮データ
+
+    for (int i = 0; i < 14; i++) {
+      homeData.insert(
+        0,
+        HomeData(DateTime.now().add(Duration(days: -i)), random.nextInt(1000)),
+      );
+    }
+
+    _homeSeriesList = [
+      Series<HomeData, DateTime>(
+        id: 'home',
+        colorFn: (_, __) => MaterialPalette.blue.shadeDefault,
+        domainFn: (HomeData data, _) => data.date,
+        measureFn: (HomeData data, _) => data.count,
+        data: homeData,
+      )
+    ];
+  }
+}
