7b6187c9b752e7f2e4989108d561f427c1b07d9a
Mori Atsushi <mori@cyder.tech>
2018-11-21 04:48:59 +0900

    sensorのrepository構造を修正

--- lib/repositorys/sensor_repository.dart
+++ lib/repositorys/sensor_repository.dart
@@ -9,18 +9,24 @@ import 'dart:async';
 import 'package:nigiru_kun/datasources/databases/model/counts.dart';
 
 abstract class SensorRepository {
-  void getCount(DateTime from, DateTime to);
-  Observable<List<NigirukunCountSensorData>> get observeCount;
+  Observable<List<NigirukunCountSensorData>> getCount(
+      DateTime from, DateTime to);
+
+  PublishSubject<List<NigirukunCountSensorData>> observeCount(
+      DateTime from, DateTime to);
+
   Observable<NigirukunCountSensorData> get observeLastInserted;
 }
 
 class SensorRepositoryImpl implements SensorRepository {
   /// Singleton
-  static final SensorRepositoryImpl _singleton = SensorRepositoryImpl._internal();
+  static final SensorRepositoryImpl _singleton =
+      SensorRepositoryImpl._internal();
   String path;
   CountProvider dbProvider = CountProvider();
-  PublishSubject<List<NigirukunCountSensorData>> _insertedStream = PublishSubject<List<NigirukunCountSensorData>>();
-  PublishSubject<NigirukunCountSensorData> _latestNigirukun = PublishSubject<NigirukunCountSensorData>();
+  PublishSubject<NigirukunCountSensorData> _latestNigirukun =
+      PublishSubject<NigirukunCountSensorData>();
+
   SensorRepositoryImpl._internal() {
     getDatabasesPath().then((value) {
       path = value + 'nigirukun.db';
@@ -29,7 +35,7 @@ class SensorRepositoryImpl implements SensorRepository {
 
     manager.countStream.listen((s) {
       for (int i = 0; i < s.count; ++i) {
-        dbProvider.insert(Count(id: null,weight: 10,time: s.time.toString()));
+        dbProvider.insert(Count(id: null, weight: 10, time: s.time.toString()));
         _latestNigirukun.add(NigirukunCountSensorData(1, s.time));
       }
     });
@@ -42,19 +48,30 @@ class SensorRepositoryImpl implements SensorRepository {
   CentralManager manager = CentralManager();
 
   @override
-  void getCount(DateTime from, DateTime to) {
-    Observable.fromFuture(dbProvider.getCount(null, null))
-      .listen((item) {
-        List<NigirukunCountSensorData> data = item.map((e) {
-          return NigirukunCountSensorData(1, DateTime.parse(e.time));
-        }).toList();
-      _insertedStream.add(data);
-    });
+  Observable<List<NigirukunCountSensorData>> getCount(
+      DateTime from, DateTime to) {
+    return Observable.fromFuture(dbProvider.getCount(from, to))
+        .map((item) => item.map((e) {
+              return NigirukunCountSensorData(1, DateTime.parse(e.time));
+            }));
   }
 
   @override
-  Observable<List<NigirukunCountSensorData>> get observeCount => _insertedStream.stream;
+  PublishSubject<List<NigirukunCountSensorData>> observeCount(
+      DateTime from, DateTime to) {
+    final PublishSubject<List<NigirukunCountSensorData>> _insertedStream =
+        PublishSubject<List<NigirukunCountSensorData>>();
+
+    _latestNigirukun.listen((_) {
+      getCount(from, to).listen((data) {
+        _insertedStream.add(data);
+      });
+    });
+
+    return _insertedStream;
+  }
 
   @override
-  Observable<NigirukunCountSensorData> get observeLastInserted => _latestNigirukun.stream;
+  Observable<NigirukunCountSensorData> get observeLastInserted =>
+      _latestNigirukun.stream;
 }
